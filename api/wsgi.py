3c6da176517edd97182431245eb5bf0907a84ff9
SamSedivy <sls325@pitt.edu>
2023-03-23 16:12:28 -0400

    delete openapi stuff

--- api/wsgi.py
+++ api/wsgi.py
@@ -1,11 +1,4 @@
-import connexion
-from api import encoder
+from app import app as application
 
-options = {"swagger_ui": False}
-app = connexion.App(__name__, specification_dir='openapi_server/openapi/', options=options)
-
-app.app.json_encoder = encoder.JSONEncoder
-app.add_api('openapi.yaml', arguments={'title': 'Ontology API'}, pythonic_params=True)
-
-# Expose global WSGI application object
-application = app.app
+if __name__ == '__main__':
+    application.run()
