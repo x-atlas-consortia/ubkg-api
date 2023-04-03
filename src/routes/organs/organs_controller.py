2b2c4f1d7f572bb49db94c979e0477f7743f3316
SamSedivy <sls325@pitt.edu>
2023-04-03 11:07:21 -0400

    Update route so an extra '/' isn't needed at the end of /organs

--- src/routes/organs/organs_controller.py
+++ src/routes/organs/organs_controller.py
@@ -3,7 +3,7 @@ from flask import Blueprint, jsonify, request, current_app
 organs_blueprint = Blueprint('organs', __name__, url_prefix='/organs')
 
 
-@organs_blueprint.route('/', methods=['GET'])
+@organs_blueprint.route('', methods=['GET'])
 def get_organ_types():
     application_context = request.args.get('application_context').upper()
     if application_context not in ['SENNET', 'HUBMAP']:
