629a3415b69b5e64672062f8b3ee6979e8cfdaa4
SamSedivy <sls325@pitt.edu>
2023-04-10 13:06:32 -0400

    Fix query string

--- src/routes/organs/organs_controller.py
+++ src/routes/organs/organs_controller.py
@@ -5,7 +5,10 @@ organs_blueprint = Blueprint('organs', __name__, url_prefix='/organs')
 
 @organs_blueprint.route('', methods=['GET'])
 def get_organ_types():
-    application_context = request.args.get('application_context').upper()
+    application_context = 'HUBMAP'
+    query_string = request.args.get('application_context')
+    if query_string is not None:
+        application_context = query_string.upper()
     if application_context not in ['SENNET', 'HUBMAP']:
         return jsonify(f'Invalid application_context specified ({application_context}) application_context query '
                        f'string must be one of SENNET or HUBMAP'), 400
