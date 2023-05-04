96690e07bb6d488c4369fd2a599dd6f563bb1aaa
SamSedivy <sls325@pitt.edu>
2023-05-04 10:58:05 -0400

    Remove default query string values and return a bad request when none are provided for the organs endpoints

--- src/routes/organs/organs_controller.py
+++ src/routes/organs/organs_controller.py
@@ -5,25 +5,19 @@ organs_blueprint = Blueprint('organs', __name__, url_prefix='/organs')
 
 @organs_blueprint.route('', methods=['GET'])
 def get_organ_types():
-    application_context = 'HUBMAP'
-    query_string = request.args.get('application_context')
-    if query_string is not None:
-        application_context = query_string.upper()
-    if application_context not in ['SENNET', 'HUBMAP']:
+    application_context = request.args.get('application_context')
+    if application_context is None:
         return jsonify(f'Invalid application_context specified ({application_context}) application_context query '
                        f'string must be one of SENNET or HUBMAP'), 400
-    return jsonify(current_app.neo4jManager.get_organ_types(application_context))
+    return jsonify(current_app.neo4jManager.get_organ_types(application_context.upper()))
 
 @organs_blueprint.route('by-code', methods=['GET'])
 def get_organ_by_code():
-    application_context = 'HUBMAP'
-    query_string = request.args.get('application_context')
-    if query_string is not None:
-        application_context = query_string.upper()
-    if application_context not in ['SENNET', 'HUBMAP']:
+    application_context = request.args.get('application_context')
+    if application_context is None:
         return jsonify(f'Invalid application_context specified ({application_context}) application_context query '
                        f'string must be one of SENNET or HUBMAP'), 400
-    data: list = current_app.neo4jManager.get_organ_types(application_context)
+    data: list = current_app.neo4jManager.get_organ_types(application_context.upper())
     result: dict = {}
     for item in data:
         result[item['rui_code']] = item['term'].strip()
