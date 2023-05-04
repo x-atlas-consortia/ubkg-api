b15a1da89727d1ff850249b7ac1211b5dc738ffe
SamSedivy <sls325@pitt.edu>
2023-05-04 11:53:42 -0400

    Validate application context on the endpoints that require it

--- src/routes/organs/organs_controller.py
+++ src/routes/organs/organs_controller.py
@@ -1,19 +1,19 @@
 from flask import Blueprint, jsonify, request, current_app, abort
 
+from routes.validate import validate_application_context
+
 organs_blueprint = Blueprint('organs', __name__, url_prefix='/organs')
 
 
 @organs_blueprint.route('', methods=['GET'])
 def get_organ_types():
-    application_context = request.args.get('application_context')
-    validate_application_context_query_string_parameter(application_context)
+    application_context = validate_application_context()
     return jsonify(current_app.neo4jManager.get_organ_types(application_context.upper()))
 
 
 @organs_blueprint.route('by-code', methods=['GET'])
 def get_organ_by_code():
-    application_context = request.args.get('application_context')
-    validate_application_context_query_string_parameter(application_context)
+    application_context = validate_application_context()
     data: list = current_app.neo4jManager.get_organ_types(application_context.upper())
     result: dict = {}
     for item in data:
@@ -21,7 +21,4 @@ def get_organ_by_code():
     return jsonify(result)
 
 
-def validate_application_context_query_string_parameter(application_context):
-    if application_context is None:
-        return abort(jsonify(f'Invalid application_context specified ({application_context}) application_context query '
-                             f'string must be one of SENNET or HUBMAP')), 400
+
