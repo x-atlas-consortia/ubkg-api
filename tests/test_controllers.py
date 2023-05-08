98ed1e069d0670734a05a9366c7a53621420eff8
SamSedivy <sls325@pitt.edu>
2023-05-08 15:12:43 -0400

    Add hubmap tests

--- tests/test_controllers.py
+++ tests/test_controllers.py
@@ -15,36 +15,65 @@ def client(get_app):
     return app.test_client()
 
 
-def assertExpectedResponseEqualsActualResponse(response, expected_response_file_name):
+def assert_expected_response_equals_actual_response(response, expected_response_file_name):
     with open(expected_response_file_name) as f:
         expected = json.loads(f.read())
     actual = response.get_json()
-    assert expected == actual
+    assert actual == expected
 
 
-def test_datasets(client):
+##region SenNet
+
+def test_sennet_datasets(client):
     response = client.get('/datasets', query_string={'application_context': 'SENNET'})
-    assertExpectedResponseEqualsActualResponse(response, 'expected_responses/datasets.json')
+    assert_expected_response_equals_actual_response(response, 'sennet_expected_responses/datasets.json')
 
 
 def test_valuest_sennet_organs(client):
     response = client.get('/organs', query_string={'application_context': 'SENNET'})
-    assertExpectedResponseEqualsActualResponse(response, 'expected_responses/valueset_organs.json')
+    assert_expected_response_equals_actual_response(response, 'sennet_expected_responses/valueset_organs.json')
 
 
 def test_valueset_sennet_entities(client):
     response = client.get('/valueset',
                           query_string={'child_sabs': 'SENNET', 'parent_sab': 'SENNET', 'parent_code': 'C000012'})
-    assertExpectedResponseEqualsActualResponse(response, 'expected_responses/valueset_entities.json')
+    assert_expected_response_equals_actual_response(response, 'sennet_expected_responses/valueset_entities.json')
 
 
 def test_valueset_sennet_specimen_categories(client):
     response = client.get('/valueset',
                           query_string={'child_sabs': 'SENNET', 'parent_sab': 'SENNET', 'parent_code': 'C020076'})
-    assertExpectedResponseEqualsActualResponse(response, 'expected_responses/valueset_specimen_categories.json')
+    assert_expected_response_equals_actual_response(response, 'sennet_expected_responses/valueset_specimen_categories.json')
 
 
 def test_valueset_sennet_source_types(client):
     response = client.get('/valueset',
                           query_string={'child_sabs': 'SENNET', 'parent_sab': 'SENNET', 'parent_code': 'C050020'})
-    assertExpectedResponseEqualsActualResponse(response, 'expected_responses/valueset_source_types.json')
+    assert_expected_response_equals_actual_response(response, 'sennet_expected_responses/valueset_source_types.json')
+
+
+##endregion
+
+##region HubMAP
+
+def test_hubmap_datasets(client):
+    response = client.get('/datasets', query_string={'application_context': 'HUBMAP'})
+    assert_expected_response_equals_actual_response(response, 'hubmap_expected_responses/datasets.json')
+
+
+def test_hubmap_organs(client):
+    response = client.get('/organs', query_string={'application_context': 'HUBMAP'})
+    assert_expected_response_equals_actual_response(response, 'hubmap_expected_responses/organs.json')
+
+
+def test_valueset_hubmap_specimen_categories(client):
+    response = client.get('/valueset', query_string={'child_sabs': 'HUBMAP', 'parent_sab': 'HUBMAP', 'parent_code': 'C020076'})
+    assert_expected_response_equals_actual_response(response, 'hubmap_expected_responses/specimen_categories.json')
+
+
+def test_valueset_hubmap_source_types(client):
+    response = client.get('/valueset',
+                          query_string={'child_sabs': 'HUBMAP', 'parent_sab': 'HUBMAP', 'parent_code': 'C050020'})
+    assert_expected_response_equals_actual_response(response, 'hubmap_expected_responses/valueset_source_types.json')
+
+##endregion
