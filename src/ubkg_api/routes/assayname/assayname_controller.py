bf4ee1e92e07cab6489de108f3da299ffd69a07f
ChuckKollar <chuck@kollar.com>
2023-06-08 12:02:12 -0400

    Request Data now follows AssayNameRequest in ubkg-api-spec.yaml

--- src/ubkg_api/routes/assayname/assayname_controller.py
+++ src/ubkg_api/routes/assayname/assayname_controller.py
@@ -1,7 +1,5 @@
 from flask import Blueprint, jsonify, current_app, request, make_response
 
-from routes.validate import validate_application_context
-
 assayname_blueprint = Blueprint('assayname', __name__, url_prefix='/assayname')
 
 
@@ -10,23 +8,31 @@ def assayname_post():
     """Get the assaytypes with name and alt-names as found in the request body with key 'name'.
     This is a replacement for search-src endpoint of the same name.
 
-    :param application_context: Filter to indicate application context
-    :type application_context: str
+    The 'application_context' is specified in the Request Data (see AssayNameRequest in ubkg-api-spec.yaml).
+    If it is not specified it will default to 'HUBMAP'.
+
+    The 'name' is also specified in the Request Data (again see AssayNameRequest in ubkg-api-spec.yaml).
 
     :rtype: Union[AssayTypePropertyInfo, Tuple[AssayTypePropertyInfo, int], Tuple[AssayTypePropertyInfo, int, Dict[str, str]]
     """
-    application_context = validate_application_context()
     if not request.is_json:
         return make_response("A JSON body with a 'Content-Type: application/json' header are required", 400)
     if 'name' not in request.json:
         return make_response('Request contains no "name" field', 400)
+    application_context = "HUBMAP"
+    if 'application_context' in request.json:
+        application_context = request.json['application_context']
     req_name = request.json['name']
-    if type(req_name) == list:
+    alt_names: list = None
+    if type(req_name) == list and len(req_name) > 0:
         name = req_name[0]
-        alt_names = req_name[1:]
+        if len(req_name) > 1:
+            alt_names = req_name[1:]
     elif type(req_name) == str:
         name = req_name
-        alt_names = None
+    else:
+        return make_response("The 'name' field is incorrectly specified "
+                             "(see AssayNameRequest in ubkg-api-spec.yaml)", 400)
     result = current_app.neo4jManager.assaytype_name_get(name, alt_names, application_context)
     if result is None:
         return make_response(f"No such assay_type {req_name}, even as alternate name", 400)
