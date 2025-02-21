cabd4f52f014318a79eeba1f85d7ca3b6b9d543b
AlanSimmons <jas971@pitt.edu>
2025-02-21 13:35:07 -0500

    refactored: removed s3worker functions from app.py

--- src/ubkg_api/utils/s3_redirect.py
+++ src/ubkg_api/utils/s3_redirect.py
@@ -22,17 +22,43 @@ def getstashurl(resp:str, s3w:S3Worker)-> flask.Response:
         err = 'Unexpected error storing large results in S3'
         return make_response(err, 500)
 
+def getconfigval(configkey:str) -> str:
+    """
+    Obtains the value for a key from the application configuration, based on
+    context.
+
+    If the ubkg-api is running standalone (development), configuration keys are
+    obtained from the ubkg-api's app.cfg.
+
+    If the ubkg-api is instantiated in a child api, configuration keys from the child api's
+    app.cfg are set as attributes of the ubkg-api instance. In particular, in case of
+    conflict, the child api's configuration takes precedence.
+
+    :param configkey: the key to search
+    """
+    configval = ''
+    if hasattr(current_app, configkey):
+        configval = current_app.configkey
+    else:
+        if configkey in current_app.config:
+            configval = current_app.config[configkey]
+
+    print('getconfigval', configkey, configval)
+    return configval
+
 def redirect_if_large(resp:str) -> flask.Response:
     """
     Checks the size of a string, assumed to be the response from an API endpoint.
 
-    If the string exceeds the size limit configured in the app.cfg, the function returns:
+    If the string does not exceed the size limit specified in configuration,
+    the function returns the string as JSON.
+
+    If the string exceeds the size limit configured in the app.cfg, the function returns
+    one of the following:
     1. If S3 redirection is specified in the app.cfg,
        a. directs the S3Worker to stash the string in a file in a specified S3 bucket
        b. returns a URL that points to the stored string
     2. If S3 redirection is not specified, returns a custom HTTP 413 response.
-
-    If the string does not exceed the size limit, the function returns the string as JSON.
 
     :param resp: a string assumed to be the response from an API endpoint.
 
@@ -40,32 +66,31 @@ def redirect_if_large(resp:str) -> flask.Response:
 
     respstr = str(resp)
 
-    print(current_app.config)
-    if 'LARGE_RESPONSE_THRESHOLD' in current_app.config:
+    threshold = getconfigval('LARGE_RESPONSE_THRESHOLD')
+    if threshold == '':
+        threshold = 0
 
-        threshold = current_app.config['LARGE_RESPONSE_THRESHOLD']
+    if threshold > 0:
 
-        if threshold > 0:
+        if getconfigval('AWS_S3_BUCKET_NAME') != '':
 
-            if 'AWS_S3_BUCKET_NAME' in current_app.config:
+            s3w = S3Worker(ACCESS_KEY_ID=getconfigval('AWS_ACCESS_KEY_ID')
+                   , SECRET_ACCESS_KEY=getconfigval('AWS_SECRET_ACCESS_KEY')
+                   , S3_BUCKET_NAME=getconfigval('AWS_S3_BUCKET_NAME')
+                   , S3_OBJECT_URL_EXPIRATION_IN_SECS=getconfigval('AWS_OBJECT_URL_EXPIRATION_IN_SECS')
+                   , LARGE_RESPONSE_THRESHOLD=getconfigval('LARGE_RESPONSE_THRESHOLD')
+                   , SERVICE_S3_OBJ_PREFIX=getconfigval('AWS_S3_OBJECT_PREFIX'))
 
-                s3w = S3Worker(ACCESS_KEY_ID=current_app.config['AWS_ACCESS_KEY_ID']
-                   , SECRET_ACCESS_KEY=current_app.config['AWS_SECRET_ACCESS_KEY']
-                   , S3_BUCKET_NAME=current_app.config['AWS_S3_BUCKET_NAME']
-                   , S3_OBJECT_URL_EXPIRATION_IN_SECS=current_app.config['AWS_OBJECT_URL_EXPIRATION_IN_SECS']
-                   , LARGE_RESPONSE_THRESHOLD=current_app.config['LARGE_RESPONSE_THRESHOLD']
-                   , SERVICE_S3_OBJ_PREFIX=current_app.config['AWS_S3_OBJECT_PREFIX'])
-
-                if len(respstr) > threshold:
+            if len(respstr) > threshold:
                     return getstashurl(resp=respstr,s3w=s3w)
 
-            else:
+        else:
 
-                # S3 redirection has not been enabled. Use default payload size checking.
-                # Return a 413 (payload too large) error if the response size exceeds the threshold.
-                err = check_payload_size(payload=respstr, max_payload_size=threshold)
-                if err != "ok":
-                    return make_response(err, 413)
+            # S3 redirection has not been enabled. Use default payload size checking.
+            # Return a 413 (payload too large) error if the response size exceeds the threshold.
+            err = check_payload_size(payload=respstr, max_payload_size=threshold)
+            if err != "ok":
+                return make_response(err, 413)
 
     # Normal return
     return jsonify(resp)
