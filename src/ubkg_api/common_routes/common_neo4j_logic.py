00259195b44acc26c28bb2b05f2e4f3050349bc1
AlanSimmons <jas971@pitt.edu>
2026-03-02 11:31:38 -0500

    Refactored /sabs/code/counts endpoints to use neo4j params.

--- src/ubkg_api/common_routes/common_neo4j_logic.py
+++ src/ubkg_api/common_routes/common_neo4j_logic.py
@@ -1166,22 +1166,22 @@ def sabs_codes_counts_query_get(neo4j_instance, sab=None, skip=None, limit=None)
     """
     sabs: list[dict] = []
 
-    # Load and parameterize query.
+    # Load query template.
     querytxt = loadquerystring('sabs_codes_counts.cypher')
-    if sab is None:
-        sabjoin = ''
-    else:
-        sabjoin = format_list_for_query(listquery=[sab], doublequote=True)
-    querytxt = querytxt.replace('$sab', sabjoin)
 
-    querytxt = querytxt.replace('$skip', str(skip))
-    querytxt = querytxt.replace('$limit', str(limit))
+    if sab is None:
+        sab = ""
+
+    # BUILD PARAMS.
+    params = {"sab": sab,
+              "skip": int(skip),
+              "limit": int(limit)}
 
     # Instantiate the query with the configured timeout.
     query = neo4j.Query(text=querytxt, timeout=neo4j_instance.timeout)
     with neo4j_instance.driver.session() as session:
         try:
-            recds: neo4j.Result = session.run(query)
+            recds: neo4j.Result = session.run(query, **params)
 
             # Track the position of the sabs in the list, based on the value of skip.
             position = int(skip) + 1
--- src/ubkg_api/cypher/sabs_codes_counts.cypher
+++ src/ubkg_api/cypher/sabs_codes_counts.cypher
@@ -2,13 +2,13 @@
 // The sab_code_count_get routine in common_neo4j_logic.py will replace the sab variable.
 
 // Optional filter on SAB
-WITH [$sab] as sab_query
+WITH $sab as sab_query
 WITH sab_query
 // Get counts of codes by SAB.
 CALL
 {
     WITH sab_query
-    OPTIONAL MATCH (n:Code) WHERE CASE WHEN sab_query=[] THEN 1=1 ELSE n.SAB in sab_query END
+    OPTIONAL MATCH (n:Code) WHERE CASE WHEN sab_query='' THEN 1=1 ELSE n.SAB = sab_query END
     RETURN n.SAB AS sab, COUNT(DISTINCT n) as code_count
     ORDER BY n.SAB
     SKIP $skip
